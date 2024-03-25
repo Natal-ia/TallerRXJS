07acb36f007ca244854321199b456609e7c0e6c2
María Andrea Méndez <101602812+mariaa-mendez@users.noreply.github.com>
2024-03-24 20:01:43 -0500

    búsqueda usuario

--- tallerRXJS/src/app/app.component.ts
+++ tallerRXJS/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './models/User';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  buttonClicked: boolean = false;
+  ROOT_URL = 'https://dummyjson.com';
   title = 'tallerRXJS';
+  txtUser: string = '';
+  usuario: User | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  searchUser() {
+
+    this.buttonClicked = true;
+
+    const url = `${this.ROOT_URL}/users/filter?key=username&value=${this.txtUser}`;
+    this.http.get<any>(url).subscribe((response: any) => {
+      if (response.users && response.users.length > 0) {
+        this.usuario = response.users[0];
+      } else {
+        this.usuario = null;
+      }
+    });
+  }
 }
---
+++ tallerRXJS/src/app/models/User.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    age: number;
+    gender: string;
+    username: string;
+    image: string;
+}
