732b1519e7ee181c5177883cecab9b139d2d513e
Natalia <nataliamejia@javeriana.edu.co>
2024-03-31 14:43:06 -0500

    Corrección de error en posts

--- tallerRXJS/src/app/posts/posts.component.ts
+++ tallerRXJS/src/app/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from '../models/Post';
 
@@ -7,15 +7,23 @@ import { Post } from '../models/Post';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
-  @Input() user: any; // Change the type as per your User model
+export class PostsComponent implements OnChanges {
+  @Input() user!: { id: number }; // Define the type of 'user'
+
   ROOT_URL = 'https://dummyjson.com';
-  publicaciones: Post[] = []; // Define the publicaciones array
+  publicaciones: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
+  
   ngOnInit() {
     if (this.user) {
+      this.getPostsByUser();
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['user'] && !changes['user'].firstChange) {
       this.getPostsByUser();
     }
   }
@@ -23,12 +31,10 @@ export class PostsComponent {
   getPostsByUser() {
     this.http.get<any>(this.ROOT_URL + '/posts/user/' + this.user.id).subscribe((response: any) => {
       if (response && response.posts) {
-        this.publicaciones = response.posts; // Assign the posts array to publicaciones
+        this.publicaciones = response.posts;
       } else {
-        this.publicaciones = []; // Handle case when there are no posts
+        this.publicaciones = [];
       }
     });
   }
-  
 }
-  
